0e2bdd60c52f1b47ea24b24ae9f62073c72a1561
ArsenAvanesyan <01borzik@gmail.com>
2024-09-06 10:19:10 +0300

    features:db-init

--- server/db/migrations/20240906051743-create-question.js
+++ server/db/migrations/20240906051743-create-question.js
@@ -10,7 +10,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       topicId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Topics',
+          key: 'id'
+        }
       },
       title: {
         allowNull: false,
